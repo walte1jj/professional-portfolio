c596ef9fcebd2c7312a1e1a3db062d510a144c54
Jermaine Walters <walte1jj@gmail.com>
2022-05-05 19:02:04 -0400

    repo final fix

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+function navigate(input) {
+    var input = input;
+
+    if (input == "taskinator") {
+        window.open("https://walte1jj.github.io/Taskinator/");
+    } else if (input == "work-day-scheduler") {
+        window.open("https://walte1jj.github.io/Work-Day-Scheduler/");
+    } else if (input == "run-buddy") {
+        window.open("https://walte1jj.github.io/run-buddy/");
+    } else if (input == "horiseon") {
+        window.open("https://walte1jj.github.io/Horiseon/");
+    } else if (input == "taskmaster-pro") {
+        window.open("https://walte1jj.github.io/taskmaster-pro/"); 
+    } else if (input == "coding-quiz") {
+        window.open("https://walte1jj.github.io/Coding-Quiz-Challange/");
+    } else {
+        return true;
+    }
+};
